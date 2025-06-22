7f74c98315e0470a3e58bc6fcbe22edc37874818
Dmitry Ivanov <ivanovdmitry91@gmail.com>
2025-06-23 02:52:08 +0300

    feat: Добавление поддержки указания версий OneScript для запуска бенчмарков

---
+++ src/BenchmarkOneScript.Extensions/ObjectExtension.cs
@@ -0,0 +1,28 @@
+﻿#if NET6_0_OR_GREATER
+using OneScript.Contexts;
+#endif
+
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace BenchmarkOneScript.Extensions;
+
+[ContextClass("РасширениеОбъекта")]
+public class ObjectExtension : AutoContext<ObjectExtension>
+{
+    [ContextProperty("ХешКод", "HashCode")]
+    public string HashCode => _Object.GetHashCode().ToString("X");
+    
+    IValue _Object;
+
+    public ObjectExtension(IValue Object)
+    {
+        _Object = Object;
+    }
+
+    [ScriptConstructor]
+    public static ObjectExtension Constructor(IValue type)
+    {
+        return new ObjectExtension(type);
+    }
+}
---
+++ src/BenchmarkOneScript.Extensions/TypeExtension.cs
@@ -0,0 +1,33 @@
+﻿#if NET6_0_OR_GREATER
+using OneScript.Contexts;
+#endif
+
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+
+namespace BenchmarkOneScript.Extensions;
+
+[ContextClass("РасширениеТипа")]
+public class TypeExtension : AutoContext<TypeExtension>
+{
+    [ContextProperty("Источник", "Source")]
+    public string Source =>
+#if NET6_0_OR_GREATER
+        AttachedScriptsFactory.GetModuleOfType(_type.ToString()).Source.Location ?? "";
+#else
+        AttachedScriptsFactory.GetModuleOfType(_type.ToString()).ModuleInfo.Origin ?? "";
+#endif
+    
+    IValue _type;
+
+    public TypeExtension(IValue type)
+    {
+        _type = type;
+    }
+
+    [ScriptConstructor]
+    public static TypeExtension Constructor(IValue type)
+    {
+        return new TypeExtension(type);
+    }
+}
